9af24d0e4dfc3920e8692dd2c5434905c3977d3c
BlakWeb <144697255+BlakWeb@users.noreply.github.com>
2024-08-27 21:40:09 +0100

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -153,9 +153,9 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
+            &copy; 2024. All Rights Reserved.
+            <a href="http://www.tiger-web-solutions.com" rel="nofollow">
+              Tiger Web Solutions.
             </a>
           </p>
         </div>
